95e813d1d6ffd890799c59fe3c64590ade3e589e
butterfly1of4 <jhouck29@gmail.com>
2020-09-21 19:25:18 -0400

    fixing portfolio

--- src/components/Portfolio/Portfolio.js
+++ src/components/Portfolio/Portfolio.js
@@ -14,35 +14,115 @@ class Portfolio extends Component {
         <h1 className="Portfolio_title">
           Portfolio<div className="Period">.</div>
         </h1>
-        <div className="project_wrapper">
-          <ul className="project_list">
-            <li className="project_item">
-              <div className="thumbnail">
-                {" "}
-                <img className="icon" src={Tank} />
-              </div>
-            </li>
-            <li className="project_item">
-              <div className="thumbnail">
-                {" "}
-                <img className="icon" src={Planet} />
-              </div>
-            </li>
-            <li className="project_item">
-              <div className="thumbnail">
-                {" "}
-                <img className="icon" src={Film} />
-              </div>
-            </li>
-          </ul>
-        </div>
-        <p className="additionalWork">
-          <h3 className="additionalTitle">Additional Work </h3>
-          <div className="additionalItems">
-            Responsive Webpage <div className="divider">|</div>Project #2{" "}
-            <div className="divider">|</div>Project #3
+        <div className="pic_text_list">
+          <div className="project_wrapper">
+            <ul className="project_list">
+              <li className="project_item">
+                <ul className="War_project">
+                  <li className="thumbnail">
+                    {" "}
+                    <img className="icon" src={Tank} />
+                  </li>
+                  <li className="links">
+                    {" "}
+                    <a
+              className="ViewCode deployed_link" target="_blank"
+                style={{ textDecoration: "none" }}
+                href="https://butterfly1of4.github.io/Game-of-War/"
+              >
+                GAME OF WAR
+              </a>
+                    <a
+                      className="ViewCode"
+                      target="_blank"
+                      style={{ textDecoration: "none" }}
+                      href="https://github.com/butterfly1of4/Game-of-War"
+                    >
+                      VIEW CODE
+                    </a>
+                  </li>
+                </ul>
+              </li>
+
+              <li className="project_item">
+                <ul className="Nasa_project">
+                  <li className="thumbnail">
+                    {" "}
+                    <img className="icon" src={Planet} />
+                  </li>
+
+                  <li className="links">
+                    {" "}
+                    <a
+                      className="ViewCode"
+                      className="deployed_link"
+                      style={{ textDecoration: "none" }}
+                      href="https://butterfly1of4.github.io/ui-pattern/"
+                    >
+                      NASA PLANETS PAGE
+                    </a>
+              
+                    <a
+                      className="ViewCode"
+                      target="_blank"
+                      style={{ textDecoration: "none" }}
+                      href="https://github.com/butterfly1of4/ui-pattern"
+                    >
+                      VIEW CODE
+                    </a>
+                  </li>
+                </ul>
+              </li>
+              <li className="project_item">
+                <ul className="Nterflix_project">
+                  <li className="thumbnail">
+                    {" "}
+                    <img className="icon" src={Film} />
+                  </li>
+                  <li className="links">
+                    {" "}
+                    <a
+                      className="ViewCode"
+                      className="deployed_link"
+                      target="_blank"
+                      style={{ textDecoration: "none" }}
+                      href="https://butterfly1of4.github.io/project8-frontend"
+                    >
+                      NTERFLIX
+                    </a>
+                
+                    <a
+                      className="ViewCode"
+                      target="_blank"
+                      style={{ textDecoration: "none" }}
+                      href="https://github.com/butterfly1of4/project8-frontend"
+                    >
+                      VIEW FRONTEND CODE
+                    </a>
+                 
+                    <a
+                      className="ViewCode"
+                      target="_blank"
+                      style={{ textDecoration: "none" }}
+                      href="https://github.com/butterfly1of4/project8-backend"
+                    >
+                      VIEW BACKEND CODE
+                    </a>
+                  </li>
+                </ul>
+              </li>
+            </ul>
           </div>
-        </p>
+
+          <p className="additionalWork">
+            <h3 className="additionalTitle">Additional Work<div className="Period">:</div></h3>
+            <div className="additionalItems">
+              Responsive Webpage{" "}<div className="divider">|</div>Project #2{" "}
+              <div className="divider">|</div>Project #3 {" "}
+              <div className="divider">|</div>Project #4 {" "}
+            </div>
+          </p>
+        </div>
       </div>
     );
   }
@@ -52,77 +132,3 @@ export default Portfolio;
 
 // https://project8-backend.herokuapp.com/expire & https://project8-backend.herokuapp.com/release
 // https://github.com/butterfly1of4/GA-responsive-webiste
-
-// <ul className="project_thumbnails">
-//             <li className="thumbnail_item">
-//               <div className="thumbnail_pic">
-//                 {" "}
-//                 <img className="thumbnail" src={Tank} />
-//               </div>
-//               {/* <p className="portfolio_links">
-//                 {" "}
-//                 <a
-//                   style={{ textDecoration: "none" }}
-//                   href="https://github.com/butterfly1of4/Game-of-War"
-//                 >
-//                   GAME OF WAR
-//                 </a>
-//               </p> */}
-//             </li>
-//             <li className="thumbnail_item">
-//               <div className="thumbnail_pic">
-//                 {" "}
-//                 <img className="thumbnail" src={Planet} />
-//               </div>
-//               {/* <p className="portfolio_links">
-//                 {" "}
-//                 <a
-//                   className="deployed_link"
-//                   style={{ textDecoration: "none" }}
-//                   href="https://butterfly1of4.github.io/ui-pattern/"
-//                 >
-//                   NASA PLANETS PAGE
-//                 </a>
-//                 <br />
-//                 <a
-//                   className="ViewCode"
-//                   style={{ textDecoration: "none" }}
-//                   href="https://github.com/butterfly1of4/ui-pattern"
-//                 >
-//                   VIEW CODE
-//                 </a>
-//               </p> */}
-//             </li>
-//             <li className="thumbnail_item">
-//               <div className="thumbnail_pic">
-//                 <img className="thumbnail" src={Film} />
-//               </div>
-//               {/* <p className="portfolio_links">
-//                 {" "}
-//                 <a
-//                   className="deployed_link"
-//                   style={{ textDecoration: "none" }}
-//                   href="https://butterfly1of4.github.io/project8-frontend"
-//                 >
-//                   NTERFLIX
-//                 </a>
-//                 <br />
-//                 <a
-//                   className="ViewCode"
-//                   style={{ textDecoration: "none" }}
-//                   href="https://github.com/butterfly1of4/project8-frontend"
-//                 >
-//                   VIEW CODE FOR FRONTEND
-//                 </a>
-//                 <br />
-//                 <a
-//                   className="ViewCode"
-//                   style={{ textDecoration: "none" }}
-//                   href="https://github.com/butterfly1of4/project8-backend"
-//                 >
-//                   VIEW CODE FOR BACKEND
-//                 </a>
-//               </p> */}
-//             </li>
-//           </ul>
-//         </div>
