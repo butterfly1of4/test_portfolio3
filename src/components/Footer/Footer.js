811bef2e66f5985d1182e2c5757560f58426ee6a
butterfly1of4 <jhouck29@gmail.com>
2020-09-24 18:34:55 -0400

    fixed footers

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -7,8 +7,9 @@ import JHLogo from "../../assets/JHLogo.png"
 import JH2 from "../../assets/JH2.png"
 import Github from "../../assets/Github.png"
 import Email from "../../assets/Email.png"
-
-
+import LinkedIN_Hover from "../../assets/LinkedIN_Hover.png"
+import Email_Hover from "../../assets/Email_Hover.png"
+import Github_Hover from "../../assets/Gethub_Hover.png"
 // React Class Component
 class Footer extends Component {
     constructor(){
@@ -23,18 +24,28 @@ class Footer extends Component {
                 <div className="Footer_icons">
                <a href="https://github.com/butterfly1of4" target="_blank">
                    {" "}
-                <div className="Github">
-                <img src={Github}  alt="Github"/>
+                <div className="container">
+                <img src={Github}  alt="Github" className="displayIcon"/>
+                <div className="overlay">
+                    <img src={Github_Hover} />
+                </div>
                 </div></a>
                 <a href="mailto:jhouck29@gmail.com" target="_blank">
-                    <div className="Footer_Email">
-                    <img src={Email} alt="Email"/>
+                    <div className="container">
+                    <img src={Email} alt="Email" className="displayIcon"/>
+                    <div className="overlay">
+                    <img src={Email_Hover} />
+                    </div>
                     </div>
                     </a>
                 <a href="https://www.linkedin.com/in/jennifer-houck/" target="_blank">
-                    <div className="LinkedIN" >
-                    <img src={LinkedIN} alt="LinkedIN"/>
-                    </div></a>
+                    <div className="container" >
+                    <img src={LinkedIN} alt="LinkedIN" className="displayIcon"/>
+                    <div className="overlay">
+                   <img src={LinkedIN_Hover}
+                 /></div>
+                    </div>
+                    </a>
                 </div>
                 <div className="Copywrite">Built by Jenn Houck <div className="Divider">|</div> © 2020</div>
                 <div className="JHLogo">
